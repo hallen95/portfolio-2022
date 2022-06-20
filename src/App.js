8d20baf23c8fe946677ac6d8d34af06116636988
BrianBitopia <brian@bitopia.digital>
2022-06-20 17:17:49 -0300

    meta og

--- src/App.js
+++ src/App.js
@@ -1,18 +1,101 @@
 import React from 'react';
+import DocumentMeta from 'react-document-meta';
 
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
 import { Navbar } from './components';
 import './App.scss';
+const meta = {
+  title: 'BDEV Porfolio | Frontend Developer © 2022',
+  description: 'This porfolio belongs to Brian Ogas, professional web develper',
+  canonical: 'https://brian-ogas.netlify.app/',
+  meta: {
+    charSet: 'utf-8',
+    name: {
+      keywords: 'react,meta,document,html,tags',
+    },
+    itemProp: {
+      name: 'The Name or Title Here',
+      description: 'This is the page description',
+      image: 'https://i.postimg.cc/rsLKkgDK/OG.jpg',
+    },
+    property: {
+      'og:title': 'BDEV Porfolio | Frontend Developer © 2022',
+      'og:type': 'article',
+      'og:image': 'https://i.postimg.cc/rsLKkgDK/OG.jpg',
+      'og:site_name':
+        'This porfolio belongs to Brian Ogas, professional web develper',
+      'og:price:amount': '19.50',
+      'og:price:currency': 'USD',
+      'twitter:site': '@site',
+      'twitter:title': 'I am a Twitter title',
+    },
+  },
+  auto: {
+    ograph: true,
+  },
+  // title: 'Brian Ogas',
+  // description: 'This porfolio belongs to Brian Ogas, professional web develper',
+  // canonical: 'https://brian-ogas.netlify.app/',
+  // meta: [
+  //   {
+  //     hid: 'facebook:title',
+  //     name: 'facebook:title',
+  //     content: 'BDEV - Frontend Developer',
+  //   },
+  //   {
+  //     hid: 'facebook:description',
+  //     name: 'facebook:description',
+  //     content: 'This porfolio belongs to Brian Ogas, professional web develper',
+  //   },
+  //   {
+  //     hid: 'facebook:image',
+  //     name: 'facebook:image',
+  //     content: { OG },
+  //   },
+  //   {
+  //     hid: 'facebook:image:alt',
+  //     name: 'facebook:image:alt',
+  //     content: 'Brian Ogas Portfolio',
+  //   },
+  //   {
+  //     hid: 'og:title',
+  //     property: 'og:title',
+  //     content: 'BDEV - Frontend Developer',
+  //   },
+  //   {
+  //     hid: 'og:description',
+  //     property: 'og:description',
+  //     content: 'This porfolio belongs to Brian Ogas, professional web develper',
+  //   },
+  //   {
+  //     hid: 'og:image',
+  //     property: 'og:image',
+  //     content: { OG },
+  //   },
+  //   {
+  //     hid: 'og:image:secure_url',
+  //     property: 'og:image:secure_url',
+  //     content: { OG },
+  //   },
+  //   {
+  //     hid: 'og:image:alt',
+  //     property: 'og:image:alt',
+  //     content: 'Brian Ogas Portfolio',
+  //   },
+  // ],
+};
 
 const App = () => (
   <div className="app">
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Testimonials />
-    <Footer />
+    <DocumentMeta {...meta}>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </DocumentMeta>
   </div>
 );
 
