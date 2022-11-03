298f62b31b33c5781682b29385513e92bbd55f50
BrianBitopia <brian@bitopia.digital>
2022-11-02 21:35:16 -0300

    social icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
+import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
 import { FaFacebookF } from 'react-icons/fa'
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/brian-ariel-ogas/"
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <BsInstagram />
+        <a>
+          <BsInstagram />
+        </a>
       </div>
       <div>
-        <FaFacebookF />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/_digitalArtisan"
+        >
+          <BsTwitter />
+        </a>
       </div>
     </div>
-  );
+  )
 }
 
 export default SocialMedia
